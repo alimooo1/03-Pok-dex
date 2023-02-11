7c6985c054aa06255bb8468ef3a4d7ebfc50ef70
alimo <124451160+alimooo1@users.noreply.github.com>
2023-02-12 01:03:38 +0330

    search added

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,23 @@
+const showItems = (items) => {
+    items.forEach((pokemon) => {
+        const newPokemon = document.createElement("div")
+        newPokemon.classList.add("card")
+        const image = document.createElement("img")
+        image.src = pokemon.image
+        const name = document.createElement("div")
+        name.classList.add("pokemon-name")
+        name.innerText= pokemon.name
+        const type = document.createElement("div")
+        type.classList.add("div")
+        type.innerText = pokemon.type
+        newPokemon.style.backgroundColor = "var(--"+pokemon.type+")"
+        newPokemon.appendChild(image)
+        newPokemon.appendChild(name)
+        newPokemon.appendChild(type) 
+        cards.appendChild(newPokemon)
+    })
+}
+
 function getPokemons(){
     var xhReq = new XMLHttpRequest();
     xhReq.open("GET", "../pokemons.json", false);
@@ -5,26 +25,19 @@ function getPokemons(){
     return JSON.parse(xhReq.responseText);
 }
 
+function searchHandler() {
+    cards.innerHTML = ""
+    const searchedArray = myPokemons.filter((checkedItem)=>{
+        return (checkedItem.name.toLowerCase()).startsWith(search.value)
+    })
+    showItems(searchedArray)
+}
+
 const myPokemons = getPokemons()
-
-console.log(myPokemons);
 const cards = document.querySelector(".cards")
+const search = document.querySelector("input")
+showItems(myPokemons)
 
-myPokemons.forEach((pokemon) => {
-    const newPokemon = document.createElement("div")
-    newPokemon.classList.add("card")
-    const image = document.createElement("img")
-    image.src = pokemon.image
-    const name = document.createElement("div")
-    name.classList.add("pokemon-name")
-    name.innerText= pokemon.name
-    const type = document.createElement("div")
-    type.classList.add("div")
-    type.innerText = pokemon.type
-    newPokemon.style.backgroundColor = "var(--"+pokemon.type+")"
-    newPokemon.appendChild(image)
-    newPokemon.appendChild(name)
-    newPokemon.appendChild(type) 
-    cards.appendChild(newPokemon)
-})
+search.addEventListener("keyup",searchHandler)
+
 
